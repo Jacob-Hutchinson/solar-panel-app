7520b6da65fff8aefa7d9ba878849154a3d09e65
Jacob Hutchinson <100453009+Jacob-Hutchinson@users.noreply.github.com>
2022-05-17 19:26:34 -0600

    Delete 5-javascript-project-solar-calculator-m5-exercise-files directory

--- 5-javascript-project-solar-calculator-m5-exercise-files/startFileModule5/js/main.js
+++
@@ -1,7 +0,0 @@
-let foundYou = document.getElementById('myForm')
-
-var selectArray = foundYou.getElementsByTagName('input')
-
-for(let i = 0; selectArray.length; i++){
-    console.log(selectArray[i].value)
-}
